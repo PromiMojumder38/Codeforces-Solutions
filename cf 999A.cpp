0fa0818e2a1def4fd7e91ff46ff74133887174ec
PromiMojumder38 <74852911+PromiMojumder38@users.noreply.github.com>
2020-12-24 19:45:41 +0600

    Add files via upload

---
+++ cf 999A.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main()
+{
+	int n, k;
+	scanf("%d %d", &n, &k);
+	
+	int a[n], i, cnt1 = 0, cnt2 = 0, flag1 = 0, flag2 = 0;
+	for(i = 0; i < n; i++) scanf("%d", &a[i]);
+	for(i = 0; i < n; i++)
+	{
+		if(a[i] > k)
+		{
+			flag1 = 1;
+			break;
+		}
+		else cnt1++;
+	}
+	
+	if(flag1 == 0) 
+	{
+		printf("%d\n", cnt1);
+		return 0;
+	}
+	
+	for(i = n-1; i >= 0; i--)
+	{
+		if(a[i] > k)
+		{
+			flag2 = 1;
+			break;
+		}
+		else cnt2++;
+	}
+	
+	if(flag1 == 1 && flag2 == 1) printf("%d\n", cnt1 + cnt2);
+}
